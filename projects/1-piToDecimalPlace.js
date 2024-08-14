55449b5c76246c98b4d8425de1ad31d310f63ca4
Dan McGarvie <danmcgarvie@gmail.com>
2024-08-14 22:34:58 +0100

    Completed first project. Pi decimal

---
+++ projects/1-piToDecimalPlace.js
@@ -0,0 +1,8 @@
+let pi="3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679"
+
+let digitNumber = Math.floor(Math.random() * 100);
+
+let digitOfPi = pi.charAt(digitNumber);
+
+console.log(digitNumber);
+console.log(digitOfPi);
