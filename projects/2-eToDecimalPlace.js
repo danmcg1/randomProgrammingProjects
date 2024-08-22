e76c3e484fc9ed3a10c0f21beb0aa2d8cf7c29d3
Dan McGarvie <danmcgarvie@gmail.com>
2024-08-22 15:27:23 +0100

    Completed project 2

---
+++ projects/2-eToDecimalPlace.js
@@ -0,0 +1,9 @@
+
+let e = "2.7182818284590452353602874713526624977572470937"
+
+let digitNumber = Math.floor(Math.random() * 100);
+
+let digitOfE = e.charAt(digitNumber);
+
+console.log(digitNumber);
+console.log(digitOfE);
