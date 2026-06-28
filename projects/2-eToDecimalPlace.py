c3fb72c1728c1d45b868100fd54d266ed3a119fb
1danmcg <danmcgarvie@gmail.com>
2026-06-28 18:53:29 +0100

    Updating e rounding

---
+++ projects/2-eToDecimalPlace.py
@@ -0,0 +1,15 @@
+# **Find PI to the Nth Digit** - 
+#  Enter a number and have the program generate PI
+#   up to that many decimal places.
+#  Keep a limit to how far the program will go.
+import math
+
+e=math.e
+
+i = int(input("Pick a number between 1 and 10: "))
+
+def eToDigit(i):
+    """Returns pi to the number generated from i"""
+    print(round(e,i))
+
+eToDigit(i)
