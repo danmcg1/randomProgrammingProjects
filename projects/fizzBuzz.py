c700e27d4f863106662a827bf025599523ebabf5
Dan McGarvie <danmcgarvie@gmail.com>
2026-06-29 15:38:04 +0100

    improving fizzbuzz

--- projects/fizzBuzz.py
+++ projects/fizzBuzz.py
@@ -7,18 +7,14 @@ def isMultipleOf3(n):
     return (n % 3 == 0)
 def isMultipleOf5(n):
     return (n % 5 == 0)
-def isMultipleOf15(n):
-    return(n % 15 ==0)
 
 for i in range(1, 100):
-    if isMultipleOf15(i) == True:
+    if isMultipleOf3(i) & isMultipleOf5(i):
         print(str(i) + "FizzBuzz")
-    elif isMultipleOf5(i) == True:
+    elif isMultipleOf5(i):
         print(str(i) + "Buzz")
-    elif isMultipleOf3(i) == True:
+    elif isMultipleOf3(i):
         print(str(i) + 'Fizz')
     else:
         print(i)
     
-
-
