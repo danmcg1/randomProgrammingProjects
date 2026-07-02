456615f3a245696d71ced60c07f34a678fc2e4f4
Dan McGarvie <danmcgarvie@gmail.com>
2026-07-02 15:02:10 +0100

    Starting on Unit converter

---
+++ projects/unitConverter.py
@@ -0,0 +1,14 @@
+
+# Unit converter: Take units of each type and convert them from Imperial to Metric, or Metric to Imperial
+# Additionally build a UI to make it easier to use
+
+conversion_rates = {
+    ( "Miles" , "Kilometers" ): 1.60934,
+    ( "Kilometers" , "Miles" ): 0.624371,
+    ( "Pounds", "Kilograms" ): 0.453592,
+    ( "Kilograms", "Pounds" ): 2.20462,
+    ( "Inches", "Centimeters"): 2.54,
+    ( "Centimeters", "Inches" ): 0.393701
+}
+
+
