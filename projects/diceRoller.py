d0c0ea20d1ecc6ec25797d9b5aad4bb454c3a5a0
1danmcg <danmcgarvie@gmail.com>
2026-07-14 21:37:43 +0100

    Reworked into functions

--- projects/diceRoller.py
+++ projects/diceRoller.py
@@ -6,27 +6,6 @@ import random
 import sys
 import time
 
-dice_pool = {}
-results = []
-results_pool = {}
-dice_type = 1
-number_of_dice = 1
-
-while dice_type > 0:
-    print("\nChoose your dice:")
-    dice_type = int(input("d4 | d6 | d8 | d10 | d12 | d20 | d100 |  d").strip() or 0)
-    if dice_type == 0:
-        print(f"\nDice selected: d20")
-    else:
-        print(f"\nDice selected: d{dice_type}")
-    number_of_dice = int(input("How many dice do you want to roll? ").strip() or 0)
-    dice_pool.update({dice_type: number_of_dice})
-
-if dice_pool == {0:0}:
-    dice_pool = {20:1}
-elif dice_pool == {0:number_of_dice}:
-    dice_pool = {20: number_of_dice}
-
 def dice_roll(type_of_dice):
     return(random.randint(1,type_of_dice))
 
@@ -36,9 +15,8 @@ def roll_dice_of_type(type_of_dice, number_of_dice):
         single_roll = dice_roll(type_of_dice)
         rolls.append(single_roll)
         number_of_dice -= 1
-    results_pool.update({type_of_dice: rolls})
+    return(rolls)
         
-
 def rolling_animation():
     start_time = time.time()
     animation_speed = 0.075
@@ -61,12 +39,42 @@ def disadvantage_rolls(results):
 def advantage_rolls(results):
     return(max(results))
 
+def get_user_dice_pool():
+    
+    dice_pool = {}
+    number_of_dice = 1
+    dice_type = 1
 
-rolling_animation()
+    while dice_type > 0:
+        print("\nChoose your dice:")
+        dice_type = int(input("d4 | d6 | d8 | d10 | d12 | d20 | d100 |  d").strip() or 0)
+        if dice_type == 0:
+            print(f"\nDice selected: d20")
+        else:
+            print(f"\nDice selected: d{dice_type}")
 
-def all_dice(dice_dict, results_dict):
-    for type_of_dice in dice_dict:
-        roll_dice_of_type(type_of_dice, dice_dict[type_of_dice])
+        number_of_dice = int(input("How many dice do you want to roll? ").strip() or 0)
+        dice_pool.update({dice_type: number_of_dice})
+
+    if dice_pool == {0:0}:
+        dice_pool = {20:1}
+    elif dice_pool == {0:number_of_dice}:
+        dice_pool = {20: number_of_dice}
+
+    return(dice_pool)
+
+
+def process_and_display_rolls(dice_dict):
+    
+    results_dict = {}
+    
+    rolling_animation()
+
+    for type_of_dice, num_dice  in dice_dict.items():
+
+        rolls = roll_dice_of_type(type_of_dice, num_dice)
+        results_dict[type_of_dice] = rolls
+
         if sum(results_dict[type_of_dice]) > 1:
             print(f"\r\nd{type_of_dice} results: {results_dict[type_of_dice]}")
         if dice_dict == {20: 2}:
@@ -79,5 +87,9 @@ def all_dice(dice_dict, results_dict):
             print(f"\rd{type_of_dice} total = {sum(results_dict[type_of_dice])}")
 
 
+def main():
+    dice_pool = get_user_dice_pool()
+    process_and_display_rolls(dice_pool)
 
-all_dice(dice_pool, results_pool)
+if __name__ == '__main__':
+    main()
