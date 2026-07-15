1729473bd4ffd3d0ff985f4bea851b3b528b35a5
Dan McGarvie <danmcgarvie@gmail.com>
2026-07-15 10:59:38 +0100

    general update

--- projects/5-nextPrimeNumber.py
+++ projects/5-nextPrimeNumber.py
@@ -2,4 +2,19 @@
 # // Have the program find prime numbers until the 
 # // user chooses to stop asking for the next one.
 
+def userSelectNumber():
+    selected_number = int(input('Select a number: '))
+    return selected_number
 
+def nextPrimeNumber(selected_number):
+    for number in range(1, selected_number):
+        if selected_number % number == 0:
+            print(number)
+        
+
+def main():
+    selected_number = userSelectNumber()
+    nextPrimeNumber(selected_number)
+
+if __name__ == '__main__':
+    main()
