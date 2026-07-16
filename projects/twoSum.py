3e937a8be45b722366badb25e614bf0cf7e162cc
Dan McGarvie <danmcgarvie@gmail.com>
2026-07-16 16:33:01 +0100

    formatting

--- projects/twoSum.py
+++ projects/twoSum.py
@@ -1,8 +1,6 @@
 
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 # You can return the answer in any order.
 
 # nums = [2,7,11,15]
@@ -31,11 +29,8 @@ class Solution(object):
                 return(results_dict[complement], index)
                 
             results_dict.update({num: index})
-            
 
-            
 solver = Solution()
-
 print(solver.twoSum(nums, target))
 
 
