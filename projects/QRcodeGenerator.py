bc993aa18d40dac559a6ae1525bc5d23c1382710
1danmcg <danmcgarvie@gmail.com>
2026-07-21 10:24:34 +0100

    work on unit coverter

--- projects/QRcodeGenerator.py
+++ projects/QRcodeGenerator.py
@@ -1,8 +1,16 @@
 
 # QR code generator: Provide a URL and have it be generated as a QR code
 
+import 
+
 value = -121
 
 print(len(str(value)))
+
+def create_qr_code():
+    return(qrcode.make('Test QR code'))
+
+print (create_qr_code())
+    
 
 
--- projects/unitConverter.py
+++ projects/unitConverter.py
@@ -11,4 +11,46 @@ conversion_rates = {
     ( "Centimeters", "Inches" ): 0.393701
 }
 
+def conversions_to_string(tuple) -> str:
+    for val in tuple:
+        result += str(val) + ' '
+    result = result.strip()
+    return(result)
 
+def get_conversion_type():
+    user_input = ''
+
+    input_message = "Pick an option:\n"
+
+    options = conversions_to_string(conversion_rates.keys())
+
+    for index, item in enumerate(options):
+        input_message += f'{index+1}) {item}\n'
+
+    input_message += 'Your choice: '
+
+    while user_input not in map(str, range(1, len(options) + 1)):
+        user_input = input(input_message)
+
+    print('You picked: ' + options[int(user_input) - 1])
+
+
+
+def unit_convertion(value, conversion) -> float:
+    converted_unit = value * conversion
+    return(converted_unit)
+
+
+def get_user_input() -> tuple:
+    conversion = get_conversion_type()
+    value = float(input(f"How many units?: "))
+    return((conversion, value))
+
+
+def main() -> float:
+    print(get_user_input())
+    # conversion_result = value * conversion
+    # return(conversion_result)
+
+if __name__ == "__main__":
+    main()
