bee60613228acea8d9657478b5d2ce28cda16894
Dan McGarvie <danmcgarvie@gmail.com>
2026-08-04 14:32:27 +0100

    Flatten image and add photos of puzzles to test

--- projects/sudokuSolver/puzzleExtractor.py
+++ projects/sudokuSolver/puzzleExtractor.py
@@ -1,5 +1,6 @@
 import cv2 as cv2 
 import easyocr as eocr
+import numpy as np
 
 def imageImport():
     image_file = 'sudoku.png'
@@ -23,7 +24,7 @@ def thresholdImage(image: list):
     return(image_thresholded)
 
 
-def getMainContour(image_thresh) -> list:
+def getMainContour(img_copy, image_thresh) -> list:
     img_copy = imageImport().copy()
     contours, hierarchy = cv2.findContours(
     image_thresh, 
@@ -41,23 +42,44 @@ def getMainContour(image_thresh) -> list:
         if len(approx) == 4:
             puzzle_contour = approx
             break
-    main_contour = cv2.drawContours(img_copy, [puzzle_contour], -1, (0, 255, 0), 5)
+    
+    return(puzzle_contour)
 
+    # main_contour = cv2.drawContours(img_copy, [puzzle_contour], -1, (0, 255, 0), 5)
+    # print(len(contours))
+    # cv2.drawContours(main_contour, contours, -1, (0, 255, 0), 20)
 
+def flattenImage(source_img, main_contour, output_size=450) -> list:
+    pts = main_contour.reshape(4,2)
+    coordinates = np.zeros((4,2), dtype="float32")
 
+    s = pts.sum(axis=1)
+    coordinates[0] = pts[np.argmin(s)]  # Top-Left has smallest sum
+    coordinates[2] = pts[np.argmax(s)]  # Bottom-Right has largest sum
 
-    print(len(contours))
-    cv2.drawContours(main_contour, contours, -1, (0, 255, 0), 20)
+    diff = np.diff(pts, axis=1)
+    coordinates[1] = pts[np.argmin(diff)]  # Top-Right has smallest difference
+    coordinates[3] = pts[np.argmax(diff)]  # Bottom-Left has largest difference
 
-    cv2.imshow("All Contours", main_contour)
+    dst = np.array([
+        [0, 0],
+        [output_size - 1, 0],
+        [output_size - 1, output_size - 1],
+        [0, output_size - 1]
+    ], dtype="float32")
+
+    matrix = cv2.getPerspectiveTransform(coordinates, dst)
+    fixed_perspective_img = cv2.warpPerspective(source_img, matrix, (output_size, output_size))
+    cv2.imshow("Fixed perspective image", fixed_perspective_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-
 def main():
-    image_data = imageImport()
-    image_thresholded = thresholdImage(image_data)
-    getMainContour(image_thresholded)
+    img_data = imageImport()
+    img_copy = imageImport()
+    image_thresholded = thresholdImage(img_data)
+    main_contour = getMainContour(img_copy, image_thresholded)
+    flattenImage(img_copy, main_contour)
 
 if __name__ == '__main__':
     main()
---
+++ projects/wordsInAString.py
@@ -0,0 +1,17 @@
+
+# Check whether a word is a palindrome with True or False being returned
+
+
+def wordsInString(string):
+    
+
+def main():
+    string = input(f"Enter a string of your choice: ")
+    print(wordsInString(string))
+
+if __name__ == '__main__':
+    main()
+            
+    
+
+
