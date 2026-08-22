75710faff2178a318c48e9409d600d8f18144f70
1danmcg <danmcgarvie@gmail.com>
2026-08-22 07:19:06 +0100

    adding in best solution

--- projects/leetCode/longestCommonPrefix.py
+++ projects/leetCode/longestCommonPrefix.py
@@ -3,7 +3,6 @@
 # If there is no common prefix, return an empty string "".
 
 strs = ["flower","flow","float"]
-strs = [""]
 
 def singleEnum(list):
     enum = []
@@ -16,20 +15,38 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        prefix = ""
-        if not strs:
-            return ""
+        prefix = strs[0]
 
-        enum = singleEnum(strs[0])
-        for i, char in enum:
-            for word in strs:
-                if i >= len(word) or word[i] != char:
-                    return prefix
-            prefix += char
+        for word in strs[1:]:
+
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+
+            if prefix == "":
+                return ""
+
         return prefix
 
 
-   
 
 answer = Solution()
 print(answer.longestCommonPrefix(strs))
+
+
+
+def mostEfficientSolution(strs):
+
+    if not strs:
+        return ""
+
+    prefix = strs[0]
+
+    for word in strs[1:]:
+
+        while not word.startswith(prefix):
+            prefix = prefix[:-1]
+
+        if prefix == "":
+            return ""
+
+    return prefix
