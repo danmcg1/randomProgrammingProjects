331d8bf5622d9bb5416e1d9dff8bfd141d7c3292
1danmcg <danmcgarvie@gmail.com>
2026-08-22 12:09:32 +0100

    deDuping array

--- projects/stringReversal.py
+++ projects/stringReversal.py
@@ -1,9 +1,10 @@
 
 def test_function():
     input = "Putting a string in here. Just like this..."
-    reversed_input = sorted(list(enumerate(input)), key=lambda tup: tup[0], reverse=True)
-    reversed_list = [i[1] for i in reversed_input]
-    output = ''.join(reversed_list)
+    output = input[::-1]
+    # reversed_input = sorted(list(enumerate(input)), key=lambda tup: tup[0], reverse=True)
+    # reversed_list = [i[1] for i in reversed_input]
+    # output = ''.join(reversed_list)
     
     return(output)
 
