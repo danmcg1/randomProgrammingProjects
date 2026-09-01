c26efaab1c91e05c48dc44c2d2bee96041726d8c
1danmcg <danmcgarvie@gmail.com>
2026-09-01 21:28:27 +0100

    addBinary compelted

--- projects/leetCode/addBinary.py
+++ projects/leetCode/addBinary.py
@@ -16,8 +16,9 @@
 #     a and b consist only of '0' or '1' characters.
 #     Each string does not contain leading zeros except for the zero itself.
 
-a = 11
-b = 1
+a = "1010"
+b = "1011"
+
 
 class Solution(object):
     def addBinary(self, a, b):
@@ -26,8 +27,38 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
+        stringDiff = len(a)-len(b)
+
+        if stringDiff > 0:
+            b = b.rjust(len(a),"0")
+        elif stringDiff <= 0:
+            a = a.rjust(len(b), "0")
+
+        a = list(a)
+        b = list(b)
+        output = [0] * max(len(a), len(b))
+
+        i = 0
+        carry = 0
+        while i < max(len(a), len(b)):
+            i += 1
+
+            total = int(a[-i]) + int(b[-i]) + carry
+
+            output[-i] = total % 2
+            carry = total // 2
+            
+            
+        if carry == 1:
+            output.insert(0, 1)
+
+        result = ''.join([str(s) for s in output])
+            
+        return result
 
         
+            
+
 
 answer = Solution()
 
