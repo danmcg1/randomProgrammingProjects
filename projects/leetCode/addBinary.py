2b34db152c81bfec605abe02be7c79003f0455a1
Dan McGarvie <danmcgarvie@gmail.com>
2026-09-03 16:10:08 +0100

    Adding in best answer

--- projects/leetCode/addBinary.py
+++ projects/leetCode/addBinary.py
@@ -54,10 +54,18 @@ class Solution(object):
 
         result = ''.join([str(s) for s in output])
             
-        return result
+        #return result
 
         
-            
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+
+        return bin(int(a,2) + int(b,2))[2:] 
 
 
 answer = Solution()
